64377c0e2b853ccb5a51dc8da822ff2324d234d9
Damian <jujute@wp.pl>
2018-07-04 11:22:40 +0200

    Added manipulation of ingredients and counting total price.

--- src/components/Burgier/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burgier/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,13 @@ import classes from './BuildControl.css';
 const buildControl = (props) => (
 	<div className={classes.BuildControl}>
 		<div className={classes.Label}>{props.label}</div>
-		<button className={classes.Less}>Add</button>
-		<button className={classes.More}>Remove</button>
+		<button
+			className={classes.Less}
+			onClick={props.added}>Add</button>
+		<button 
+			className={classes.More}
+			onClick={props.removed}
+			disabled={props.disabled}>Remove</button>
 	</div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Auxilliary';
 import Burgier from '../../components/Burgier/Burgier';
 import BuildControls from '../../components/Burgier/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+	salad: 0.5,
+	cheese: 0.4,
+	meat: 1.3,
+	bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
 
 	state = {
@@ -12,14 +19,59 @@ class BurgerBuilder extends Component {
 			bacon: 0,
 			cheese: 0,
 			meat: 0
+		},
+		totalPrice: 4
+	}
+
+	addIngredientHandler = (type) => {
+		const oldQuantity = this.state.ingredients[type];
+		const updatedQuantity = oldQuantity + 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+
+		updatedIngredients[type] = updatedQuantity;
+		const priceAddition = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice + priceAddition;
+
+		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+	}
+
+	removeIngredientHandler = (type) => {
+		const oldQuantity = this.state.ingredients[type];
+		if (oldQuantity <= 0) {
+			return;
 		}
+		const updatedQuantity = oldQuantity - 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+
+		updatedIngredients[type] = updatedQuantity;
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - priceDeduction;
+
+		this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
 	}
 
 	render () {
+		const disabledButton = {
+			...this.state.ingredients
+		};
+
+		for (let key in disabledButton) {
+			disabledButton[key] = disabledButton[key] <= 0
+		}
 		return (
 			<Aux>
 				<Burgier ingredients={this.state.ingredients} />
-				<BuildControls />
+				<BuildControls
+				ingredientAdded = {this.addIngredientHandler}
+				ingredientRemoved = {this.removeIngredientHandler}
+				disabled = {disabledButton}
+				price = {this.state.totalPrice} />
 			</Aux>
 		);
 	}
