cd0da3e5cd7c4b5eef908ce1cff80c2c9edfe3c0
Damian <jujute@wp.pl>
2018-07-08 12:48:44 +0200

    Added navigation styling and a logo instead of placeholders. Setup everything as mobile-first. Side drawer also is added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxilliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 import classes from './Layout.css';
 
 const layout = ( props ) => (
 	<Aux>
-		<div>Tooblar, SideDrawer, Backdrop</div>
+		<Toolbar />
+		<SideDrawer />
 
 		<main className={classes.Content}>
 			{props.children}
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -81,6 +81,10 @@ class BurgerBuilder extends Component {
 		this.setState({purchasing: false});
 	}
 
+	purchaseContinueHandler = () => {
+		alert('Next step is being built...');
+	}
+
 	render () {
 		const disabledButton = {
 			...this.state.ingredients
@@ -92,7 +96,11 @@ class BurgerBuilder extends Component {
 		return (
 			<Aux>
 				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-					<OrderSummary ingredients={this.state.ingredients} />
+					<OrderSummary 
+						ingredients={this.state.ingredients}
+						price={this.state.totalPrice}
+						purchaseCancelled={this.purchaseCancelHandler}
+						purchaseContinued={this.purchaseContinueHandler} />
 				</Modal>
 				<Burgier ingredients={this.state.ingredients} />
 				<BuildControls
